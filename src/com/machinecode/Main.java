45ce56266048053c3c837c0c69fa95b09efb3081
Cinton Yeboah <clintonyeb@gmail.com>
2020-04-16 23:56:39 -0500

    changed name to judit

--- src/com/machinecode/Main.java
+++ src/com/machinecode/Main.java
@@ -3,6 +3,6 @@ package com.machinecode;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Machine Code");
+        System.out.println("Machine Judit");
     }
 }
